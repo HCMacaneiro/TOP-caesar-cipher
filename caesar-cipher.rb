738182ed7277bfb49406ec43be58597cb9c0288e
Henrique <87825553+HCMacaneiro@users.noreply.github.com>
2024-12-15 19:46:42 -0300

    Add logic for handling bigger shifts

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,20 +1,14 @@
 def caesar_cipher(word, num_shift)
   letters = word.split("")
   letters_after_shift = letters.map do |letter|
-    if (letter.ord < 65) || (letter.ord > 90 && letter.ord < 97) || (letter.ord > 122)
+    if letter.ord < 65 || letter.ord.between?(90, 96) || letter.ord > 122
       letter
     else
       ascii_letter = letter.ord
       if ascii_letter < 90
-        cipher_ascii_letter = ascii_letter + num_shift
-        if cipher_ascii_letter > 90
-          cipher_ascii_letter -= 26
-        end
+        cipher_ascii_letter = (((ascii_letter - 65) + num_shift) % 26) + 65
       else
-        cipher_ascii_letter = ascii_letter + num_shift
-        if cipher_ascii_letter > 122
-          cipher_ascii_letter -= 26
-        end
+        cipher_ascii_letter = (((ascii_letter - 97) + num_shift) % 26) + 97
       end 
       cipher_letter = cipher_ascii_letter.chr
       cipher_letter
